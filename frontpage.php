fca67e0f947fa9260f26eb7e00e718d0454f9ab5
wadegoren <79055042+wadegoren@users.noreply.github.com>
2023-04-06 20:13:29 -0400

    Add files via upload

--- frontpage.php
+++ frontpage.php
@@ -5,15 +5,15 @@
 
 <body>
     <form method="POST" action="inputDate.php">
-      <label for="date">Enter Date:</label>
-      <input type="date" id="date" name="date">
-      <button type="submit">Submit</button>
-    </form>
-
-    <button onclick="showPopup()">Add Customer</button>
+      <body for="date">Enter Date:</body>
+      <input type="date" id="date" name="date"><br><br>
+      <button  class="button"type="submit">Submit</button>
+    </form><br>
+    <h2>Add New Customer:</h2>
+    <button  class="button" onclick="showPopup()">Add Customer</button>
 
     <h2>Order Information:</h2>
-  </body>
+
 
 <?php 
 include 'connectdb.php';
@@ -27,15 +27,17 @@ $stmt->execute();
 $results = $stmt->fetchAll(); 
 // display results
 if ($stmt->rowCount() > 0) {
-  echo "<table><tr><th>Order Date</th><th>Number of Orders</th></tr>";
+  echo "<table style='margin: auto;'>";
+  echo "<tr><th>Order Date</th><th>Number of Orders</th></tr>";
   foreach ($results as $row) {
-      echo "<tr><td>".$row["dateOrder"]."</td><td>".$row["numOrders"]."</td></tr>";
+      echo "<tr><td>" . $row["dateOrder"] . "</td><td>" . $row["numOrders"] . "</td></tr>";
   }
   echo "</table>";
 } else {
   echo "No food orders found";
 }
-?>
+?><br>
+<h2>View Employee Schedule:</h2>
 
 <?php
 $query = "SELECT firstName, lastName FROM employee";
@@ -50,11 +52,15 @@ $employees = $random->fetchAll();
 echo "<form method='POST' placeholder='Select'>";
 echo "<select name='employee_name' style='width: 200px; height: 30px'>";
 foreach ($employees as $row) {
-    $employee_name = $row['firstName'] . ' ' . $row['lastName'];
-    echo "<option value='" . $employee_name . "'>" . $employee_name . "</option>";
+  $employee_name = $row['firstName'] . ' ' . $row['lastName'];
+  $selected = '';
+  if (isset($_POST['employee_name']) && $_POST['employee_name'] == $employee_name) {
+      $selected = 'selected';
+  }
+  echo "<option value='" . $employee_name . "' " . $selected . ">" . $employee_name . "</option>";
 }
-echo "</select>";
-echo "<input type='submit' name='submit' value='Select Employee'>";
+echo "</select><br>";
+echo "<input  class='button' type='submit' name='submit' value='View Schedule'>";
 echo "</form>";
 
 // Handle form submission
@@ -83,12 +89,13 @@ if(isset($_POST['submit'])) {
     $schedule = $stmt->fetchAll();
   
     // Display the schedule in a table format
-    echo "<table>";
-    echo "<tr><th>Day</th><th>Start Time</th><th>End Time</th></tr>";
+    echo "<br><table style='margin: auto;'>";
+    echo "<tr><th>Day</th><th style='padding-right: 20px;'>Start Time</th><th>End Time</th></tr>";
     foreach ($schedule as $row) {
-        echo "<tr><td>" . $row['day'] . "</td><td>" . $row['startTime'] . "</td><td>" . $row['endTime'] . "</td></tr>";
-    }
-    echo "</table>";
+      echo "<tr><td>" . $row['day'] . "</td><td>" . $row['startTime'] . "</td><td>" . $row['endTime'] . "</td></tr>";
+}
+echo "</table>";
+
 }
 ?>
 
@@ -105,6 +112,32 @@ if(isset($_POST['submit'])) {
     }
   </script>
   <style>
+  body {
+    text-align: center;
+    font-family: Arial, sans-serif;
+  }
+
+  h1 {
+    font-family: Arial, sans-serif;
+  }
+
+  h2 {
+    font-family: Arial, sans-serif;
+  }
+
+  table {
+    style='margin: auto;
+    text-align: center;
+    font-family: Arial, sans-serif;
+    font-size: 18px;
+  }
+
+
+  table th {
+  padding: 10px;
+  font-size: 18px;
+}
+
     #popup {
       display: none;
       position: fixed;
@@ -114,6 +147,7 @@ if(isset($_POST['submit'])) {
       height: 100%;
       background-color: rgba(0, 0, 0, 0.5);
       z-index: 1;
+      border-radius: 8px;
     }
 
     #popup-content {
@@ -122,42 +156,77 @@ if(isset($_POST['submit'])) {
       left: 50%;
       transform: translate(-50%, -50%);
       background-color: white;
-      padding: 20px;
+      padding: 100px;
       border-radius: 5px;
     }
 
+    #popup-content label {
+    font-size: 18px;
+    margin-bottom: 10px;
+    display: block;
+    padding: 10px;
+  }
+
+  #popup-content input {
+    font-size: 18px;
+    padding: 10px;
+    border-radius: 10px;
+    width: 500px;
+    box-sizing: border-box;
+  }
+
+  .button {
+  display: inline-block;
+  background-color: #4CAF50;
+  border: none;
+  color: white;
+  padding: 8px 10px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  border-radius: 5px;
+  cursor: pointer;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+}
+
+.button:hover {
+  background-color: #3e8e41;
+  box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.3);
+}
+
   </style>
-<body>
 
-  <div id="popup">
-    <div id="popup-content">
-      <h2>Add Customer</h2>
-      <form method="POST" action="addCustomer.php">
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required>
 
-        <label for="firstName">First Name:</label>
-        <input type="text" id="firstName" name="firstName" required>
+<div id="popup">
+  <div id="popup-content">
+    <h2 style='font-size:25px;'>Input Customer Information</h2>
+    <form method="POST" action="addCustomer.php">
+      <label for="email">Email:</label>
+      <input type="email" id="email" name="email" required style="display: block;"><br>
 
-        <label for="lastName">Last Name:</label>
-        <input type="text" id="lastName" name="lastName" required>
+      <label for="firstName">First Name:</label>
+      <input type="text" id="firstName" name="firstName" required style="display: block;"><br>
 
-        <label for="phone">Phone:</label>
-        <input type="tel" id="phone" name="phone" required>
+      <label for="lastName">Last Name:</label>
+      <input type="text" id="lastName" name="lastName" required style="display: block;"><br>
 
-        <label for="streetAddress">Street Address:</label>
-        <input type="text" id="streetAddress" name="streetAddress" required>
+      <label for="phone">Phone:</label>
+      <input type="tel" id="phone" name="phone" required style="display: block;"><br>
 
-        <label for="city">City:</label>
-        <input type="text" id="city" name="city" required>
+      <label for="streetAddress">Street Address:</label>
+      <input type="text" id="streetAddress" name="streetAddress" required style="display: block;"><br>
 
-        <label for="pc">Postal Code:</label>
-        <input type="text" id="pc" name="pc" required>
+      <label for="city">City:</label>
+      <input type="text" id="city" name="city" required style="display: block;"><br>
 
-        <button type="submit">Add Customer</button>
-      </form>
-      <button onclick="hidePopup()">Close</button>
-    </div>
+      <label for="pc">Postal Code:</label>
+      <input type="text" id="pc" name="pc" required style="display: block;">
+      <br>
+      <button class="button" type="submit">Add Customer</button> 
+    </form>
+    <br>
+    <button class="button" onclick="hidePopup()">Close</button>
   </div>
+</div>
 </body>
 </html>
--- inputDate.php
+++ inputDate.php
@@ -8,7 +8,7 @@ $date = $_POST['date'];
 $date = filter_var($date, FILTER_SANITIZE_STRING);
 
 // retrieve foodorder made on the specified date
-$sql = "SELECT customeraccount.firstName, customeraccount.lastName, food.name, foodOrder.tip, employee.firstName AS deliveryFirstName, employee.lastName AS deliveryLastName
+$sql = "SELECT customeraccount.firstName, customeraccount.lastName, foodOrder.totalPrice, food.name, foodOrder.tip, employee.firstName AS deliveryFirstName, employee.lastName AS deliveryLastName
 FROM CustomerAccount JOIN OrderPlacement on CustomerAccount.emailAddress = OrderPlacement.customerEmail
 JOIN foodOrder on OrderPlacement.OrderID = foodOrder.orderID
 JOIN foodItemsinOrder on foodOrder.orderID = foodItemsinOrder. orderID
@@ -24,17 +24,63 @@ $stmt->execute();
 
 // display results
 if ($stmt->rowCount() > 0) {
-    echo "<table><tr><th>First Name</th><th>Last Name</th><th>Items Ordered</th><th>Total Amount</th><th>Tip</th><th>Delivery Person</th></tr>";
+    echo "<div style='margin: auto; width: 300px; font-family: Arial, sans-serif;'>";
+    echo "<h1 style='text-align: center;'>Receipt</h1>";
+    echo "<p style='text-align: center;'>Date: " . date('Y-m-d') . "</p>";
+    echo "<table style='border-collapse: collapse; width: 100%;'>";
+    echo "<thead><tr><th style='text-align: center;'>Item</th><th style='text-align: center;'>Total Price</th></tr></thead>";
+    echo "<tbody>";
+    $total = 0;
     while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        echo "<tr><td>".$row["firstName"]."</td><td>".$row["lastName"]."</td><td>".$row["name"]."</td><td>".$row["tip"]."</td><td>".$row["tip"]."</td><td>".$row["deliveryFirstName"]." ".$row["deliveryLastName"]."</td></tr>";
+        echo "<tr><td style='text-align: center;'>".$row["name"]."</td><td style='text-align: center;'>$".$row["totalPrice"]."</td></tr>";
+        $total += $row["totalPrice"];
+        $tip = $row["tip"];
     }
-    echo "</table>";
+    echo "<tr><td style='text-align: center;'>Tip</td><td style='text-align: center;'>$".$tip."</td></tr>";
+    $total += $tip;
+    echo "<tr><td style='text-align: center; font-weight: bold;'>Total</td><td style='text-align: center; font-weight: bold;'>$".$total."</td></tr>";
+    echo "</tbody></table>";
+    echo "<p style='text-align: center;'>Thank you for your order!</p>";
+    echo "</div>";
 } else {
-    echo "No food order found for this date";
+    echo "<p>No food order found for this date</p>";
 }
+
+
 
 // close connection
 $conn = null;
-?>
+?><br>
 
-<a  href="frontpage.php">Return to homepage</a>
+<style>
+    a {
+        display: block;
+        margin: auto;
+        font-family: Arial, sans-serif;
+        font-size: 18px;
+        text-align: center;
+    }
+
+    
+  table {
+    style='margin: auto;
+    text-align: center;
+    font-family: Arial, sans-serif;
+    font-size: 18px;
+  }
+
+    table th {
+  padding: 10px;
+  font-size: 18px;
+  font-family: Arial, sans-serif;
+  
+}
+
+table tb {
+  padding: 10px;
+  font-size: 18px;
+  font-family: Arial, sans-serif;
+}
+</style>
+
+<a href="frontpage.php">Return to homepage</a>
